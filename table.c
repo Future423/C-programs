8e3f718eb85295c94e35f0efa7908ac9bad84c4b
Sukuna1003 <145987937+Sukuna1003@users.noreply.github.com>
2023-10-01 13:55:00 +0530

    Update table.c

--- table.c
+++ table.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<conio.h>
-main()
+void main()
 {
   int i,n;
   clrscr();
-  printf("Enter a number");
+  printf("Enter a number ");
   scanf("%d",&n);
-  for(i=1;i=11;i++)
-    printf("%d x %d = %d",n,i,n*i);
+  for(i=1;i<=10;i++)
+    printf("%3d x %2d = %3d\n",n,i,n*i);
   getch();
+}
